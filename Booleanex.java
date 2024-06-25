11d9635eceae65c681635b05317bb4ac276b64eb
Prem Bhalerao <sb.prem17@gmail.com>
2024-06-25 17:39:43 +0530

    file add

---
+++ Booleanex.java
@@ -0,0 +1,13 @@
+public class Booleanex{
+    public static void main(String []args){
+        int myAge = 25;
+        int votingAge = 18;
+        if(myAge >= votingAge){
+            System.out.println("old enough to vote");
+        } 
+        else{
+            System.out.println("not old enough to vote");
+                    
+        }
+    }
+}
